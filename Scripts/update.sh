3560ffe68c72e318604b5636dfe062459cef056c
Hassan Fakih <hasanfakih@cmail.carleton.ca>
2026-09-07 14:23:03 -0400

    Update JustBot Config

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -1,7 +1,9 @@
   git fetch
   git pull --ff-only origin master
   source .venv/bin/activate
-  source /etc/openbench.env
+  set -a
+  source <(sudo cat /etc/openbench.env)
+  set +a
   pip install -r requirements.txt
   python3 manage.py check --settings=OpenSite.settings_prod
   python3 manage.py migrate --settings=OpenSite.settings_prod
